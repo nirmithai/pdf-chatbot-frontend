70a6841373fbe4b17c1c61e18ee536d0b791cd61
sudhanvateggihal <sudhanvateggihal@gmail.com>
2025-03-14 13:43:46 +0530

    Refactor project structure: remove App component, update index to use Demo, and enhance styling in index.css

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Demo from './demo';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Demo/>
   </React.StrictMode>
 );
 
@@ -15,3 +17,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+  
